0808b4a298ac93a13501d3e1990fd0c73e00d5b7
Terry Tay <terry@resynctech.com>
2021-03-07 20:51:21 +0800

    Add subsink package

--- src/app/posts/components/posts-page/posts.component.ts
+++ src/app/posts/components/posts-page/posts.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {Post} from "../../models/posts.model";
 import {Observable} from "rxjs";
 import {PostsService} from "../../services/posts.service";
 import {addPost, removePost, retrievedPosts} from "../../../store/posts/posts.actions";
 import {select, Store} from "@ngrx/store";
 import { selectPosts } from 'src/app/store/posts/posts.selector';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnDestroy {
   posts$: Observable<Post[]> = this.store.pipe(select(selectPosts));
+
+  subsink = new SubSink();
 
   constructor(private postsService: PostsService, private store: Store) { }
 
   ngOnInit(): void {
-    this.postsService.getPosts()
+    this.subsink.sink = this.postsService.getPosts()
       .subscribe((posts: Post[]) => this.store
         .dispatch(
         retrievedPosts({ posts })
@@ -31,6 +34,10 @@ export class PostsComponent implements OnInit {
 
   onRemove(postId: string) {
     this.store.dispatch( removePost({ postId }) );
+  }
+
+  ngOnDestroy() {
+    this.subsink.unsubscribe();
   }
 
 }
